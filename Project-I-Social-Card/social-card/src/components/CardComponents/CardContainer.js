b644dcb430207aa2076fbd76c48ff60a649cb36d
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-11 14:49:43 -0500

    Adds unstyled CardContainer with content

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,4 +1,11 @@
 import React from "react"
 import "./Card.css"
+import CardBanner from "./CardBanner"
+import CardContent from "./CardContent"
 
-export default () => <div>Empty card here</div>
+export default () => (
+  <section>
+    <CardBanner src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" />
+    <CardContent />
+  </section>
+)
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,13 @@
-import React from "react"
+import React, { Fragment } from "react"
 import "./Card.css"
+
+export default () => (
+  <Fragment>
+    <h3>Get started with React</h3>
+    <p>
+      React makes it painless to create interactive UIs. Design simple views for
+      eact state in your application
+    </p>
+    <div className="link">reactjs.org</div>
+  </Fragment>
+)
