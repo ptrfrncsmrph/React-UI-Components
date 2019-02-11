e153aa3573ee1d07b36934660a81ea926afc17be
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-11 15:01:20 -0500

    Adds minimal styling to HeaderContainer and CardContainer

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -4,8 +4,10 @@ import CardBanner from "./CardBanner"
 import CardContent from "./CardContent"
 
 export default () => (
-  <section>
-    <CardBanner src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" />
-    <CardContent />
-  </section>
+  <div className="card-container">
+    <section>
+      <CardBanner src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" />
+      <CardContent />
+    </section>
+  </div>
 )
