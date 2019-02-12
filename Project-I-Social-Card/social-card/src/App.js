65a12d68fb19afec70bdfbece71a201189ae2e8f
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-11 21:06:15 -0500

    Adds Context API for likes and retweets

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,15 +1,34 @@
-import React, { Fragment } from "react"
+import React, { createContext, Component } from "react"
 import CardContainer from "./components/CardComponents/CardContainer"
 import "./App.css"
 import HeaderContainer from "./components/HeaderComponents/HeaderContainer"
 import Footer from "./components/FooterComponents/Footer"
 
-const App = () => (
-  <div className="container">
-    <HeaderContainer />
-    <CardContainer />
-    <Footer />
-  </div>
-)
+export const TweetStats = createContext()
+
+class App extends Component {
+  state = {
+    likes: 0,
+    retweets: 0
+  }
+  increment(key) {
+    this.setState(({ [key]: k }) => ({
+      [key]: k + 1
+    }))
+  }
+  render() {
+    return (
+      <TweetStats.Provider
+        value={{ ...this.state, increment: this.increment.bind(this) }}
+      >
+        <div className="container">
+          <HeaderContainer />
+          <CardContainer />
+          <Footer />
+        </div>
+      </TweetStats.Provider>
+    )
+  }
+}
 
 export default App
--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,16 +1,35 @@
 import React from "react"
 import "./Footer.css"
 
+import { TweetStats } from "../../App"
+
 import heart from "../../assets/heart-regular.svg"
 import comment from "../../assets/comment-regular.svg"
 import retweet from "../../assets/retweet-solid.svg"
 import envelope from "../../assets/envelope-regular.svg"
 
 export default () => (
-  <footer>
-    <img src={comment} />
-    <img src={retweet} />
-    <img src={heart} />
-    <img src={envelope} />
-  </footer>
+  <TweetStats.Consumer>
+    {context => (
+      <footer>
+        <div className="icon comment">
+          <img src={comment} />
+        </div>
+        <div
+          className="icon retweet"
+          onClick={() => context.increment("retweets")}
+        >
+          <img src={retweet} />
+          {context.retweets}
+        </div>
+        <div className="icon heart" onClick={() => context.increment("likes")}>
+          <img src={heart} />
+          {context.likes}
+        </div>
+        <div className="icon envelope">
+          <img src={envelope} />
+        </div>
+      </footer>
+    )}
+  </TweetStats.Consumer>
 )
